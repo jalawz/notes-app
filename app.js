2cd1261ee76262ab5e9af4808b635f16606743cb
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2019-08-26 14:13:55 -0300

    Module 3/05

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const notes = require('./notes');
 
 console.log(notes());
 
 console.log(validator.isEmail("gmail.com"));
 console.log(validator.isURL("https/www.gmail.com"));
+console.log(chalk.green.bold("Success!"));
