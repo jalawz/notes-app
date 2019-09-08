57dc2dcc8fe06689dd63511d9053b1ab4ff4759f
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2019-09-08 20:32:43 -0300

    Module 04/06

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-const chalk = require('chalk');
-const yargs = require('yargs');
-const notes = require('./notes');
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes");
 
 // Customize yargs version
-yargs.version('1.1.0');
+yargs.version("1.1.0");
 
 // Create add command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
     },
-    handler: function(argv) {
-        console.log(`Title: ${argv.title} - Body: ${argv.body}`);
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
     }
+  },
+  handler: function(argv) {
+    notes.addNote(argv.title, argv.body);
+  }
 });
 
 // Create remove command
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    handler: function() {
-        console.log('Removing the note');
-    }
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing the note");
+  }
 });
 
 // Create list command
 yargs.command({
-    command: 'list',
-    describe: 'Listing notes',
-    handler: function() {
-        console.log('Listing notes');
-    }
+  command: "list",
+  describe: "Listing notes",
+  handler: function() {
+    console.log("Listing notes");
+  }
 });
 
 // Create read command
 yargs.command({
-    command: 'read',
-    describe: 'Reading a note',
-    handler: function() {
-        console.log('Reading a single note');
-    }
+  command: "read",
+  describe: "Reading a note",
+  handler: function() {
+    console.log("Reading a single note");
+  }
 });
 
 yargs.parse();
--- notes.js
+++ notes.js
@@ -1,5 +1,44 @@
+const fs = require("fs");
+
 const getNotes = () => {
-    return "Your notes...";
+  return "Your notes...";
 };
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note => {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body
+    });
+    saveNotes(notes);
+    console.log("New note added");
+  } else {
+    console.log("Note title taken");
+  }
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
+  } catch (e) {
+    console.log("No data yet");
+    return [];
+  }
+};
+
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+module.exports = {
+  getNotes,
+  addNote
+};
